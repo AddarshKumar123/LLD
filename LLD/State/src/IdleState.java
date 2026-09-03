20456820e578e87bcbdd90d0df449b863c312ffe
AddarshKumar123 <addarshkumar2005@gmail.com>
2026-09-04 00:49:49 +0530

    added state design pattern

---
+++ LLD/State/src/IdleState.java
@@ -0,0 +1,18 @@
+public class IdleState implements MachineState{
+
+    @Override
+    public void selectItem(VendingMachine context, String itemCode) {
+        System.out.println("Item selected: " + itemCode);
+        context.set
+    }
+
+    @Override
+    public void insertCoin(VendingMachine context, double amount) {
+
+    }
+
+    @Override
+    public void dispenseItem(vendingMachine context) {
+
+    }
+}
